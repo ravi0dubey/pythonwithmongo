7cbfe7ce3261903a490d0a94e7c5021be2a7bc56
ravi0dubey <ravi0dubey@gmail.com>
2022-07-17 09:58:50 -0400

    additional changes

--- mongodbconnect.py
+++ mongodbconnect.py
@@ -3,13 +3,14 @@
 # $ python -m pip install 'pymongo[srv]'
 
 import pymongo
-client = pymongo.MongoClient("mongodb+srv://ravi0dubey:Logi1234#@cluster0.9hjidow.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-print(db)
 
-# client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+# client = pymongo.MongoClient("mongodb+srv://ravi0dubey:Logi1234#@cluster0.9hjidow.mongodb.net/?retryWrites=true&w=majority")
 # db = client.test
 # print(db)
+
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
 
 d1 = {
     "fname":"Ravi Dubey",
@@ -18,6 +19,16 @@ d1 = {
 
 }
 db1 = client['mongotest1']
-
 coll= db1['test']
 coll.insert_one(d1)
+
+
+d2 = {
+    "fname":"Ravi Dubey",
+    "lname":"Dubey",
+    "email": "r@gmail.com",
+
+}
+db2 = client['mongotest1']
+coll= db2['test']
+coll.insert_one(d2)
