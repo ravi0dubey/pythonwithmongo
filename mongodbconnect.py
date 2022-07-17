56fafefbad370f11f05ab1e6678989431ddfa3cd
ravi0dubey <ravi0dubey@gmail.com>
2022-07-17 09:56:05 -0400

    initial changes

---
+++ mongodbconnect.py
@@ -0,0 +1,23 @@
+#install pymongo and pymongo[srv]
+# pip install pymongo
+# $ python -m pip install 'pymongo[srv]'
+
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ravi0dubey:Logi1234#@cluster0.9hjidow.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+# client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+# db = client.test
+# print(db)
+
+d1 = {
+    "fname":"Ravi Dubey",
+    "lname":"Dubey",
+    "email": "r@gmail.com",
+
+}
+db1 = client['mongotest1']
+
+coll= db1['test']
+coll.insert_one(d1)
